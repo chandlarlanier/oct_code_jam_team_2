49ca15c09006b140c89243c2e6f23af61e362ff0
Ian Kamar <ian.kamar@outlook.com>
2023-10-24 17:48:30 -0400

    update my project

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,13 +1,7 @@
 import "./MovieCard.css";
 
-const MovieCard = ({ movie }) => {
-  return (
-    <div className="movie-card">
-      <img src={movie.posterUrl} alt={movie.title} />
-      <h3>{movie.title}</h3>
-      <p>{movie.releaseYear}</p>
-    </div>
-  );
+const MovieCard = () => {
+  return <div></div>;
 };
 
 export default MovieCard;
