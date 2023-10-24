5596e8f565d8a0b631e611f628be39b5bebcf549
Ian Kamar <ian.kamar@outlook.com>
2023-10-24 10:44:02 -0400

    update my project

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>Developed by Team INC</div>
+      <div>2023</div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,18 @@
+import React from "react";
+import logo from "../logo.svg";
 import "./Header.css";
 
 const Header = () => {
-    <div>
-        
-    </div>
-}
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="header__description">
+        <p>Discover the spookiest movies for your Halloween night!</p>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
