ca38e1c8e7ffc85529fd459027b88f7c24a3285f
Ian Kamar <ian.kamar@outlook.com>
2023-10-24 17:38:25 -0400

    update my project

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,15 @@
-import logo from '../logo.svg';
-import './App.css';
+// import logo from "../logo.svg";
+import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,9 +1,13 @@
 import "./MovieCard.css";
 
-const MovieCard = () => {
-    <div>
-
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <img src={movie.posterUrl} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <p>{movie.releaseYear}</p>
     </div>
-}
+  );
+};
 
 export default MovieCard;
