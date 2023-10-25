5072555ab4606e1dabdb3f12674bcbce7a06feb9
Chandlar Lanier <125407723+chandlarlanier@users.noreply.github.com>
2023-10-24 21:42:35 -0400

    Update Main.js

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,25 @@
 import "./Main.css";
+import MovieCard from "../MovieCard/MovieCard";
+import rightArrow from "../../images/right-arrow.svg";
+import leftArrow from "../../images/left-arrow.svg";
 
 const Main = () => {
-    <div>
-
+  return (
+    <div className="main">
+      <div className="main__app-description">Welcome to Haunted Tomatoes, your one-stop destination for all things spooky and cinematic! Get ready to dive into the world of Halloween movies, share your opinions, and discover new frightful favorites.</div>
+      <div className="main__movie-cards">
+        <div className="movie-cards__left-scroll"><img className="left-scroll__arrow" src={leftArrow}/></div>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <div className="movie-cards__right-scroll">
+            <img className='right-scroll__arrow' src={rightArrow}/>
+        </div>
+      </div>
+      <div className="main__movie-description">Selected movie description will go here.</div>
     </div>
-}
+  );
+};
 
 export default Main;
