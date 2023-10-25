3f5a5fa044fbeb13bef0b87798eb4e53404c8f77
Chandlar Lanier <cklanier@outlook.com>
2023-10-25 18:59:27 -0400

    Added movies to constants

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -3,26 +3,66 @@ export const movieArray = [
   {
     title: "Monster House",
     card: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zCRPr4bkO3ae0U1134vJ39xZnAG.jpg",
-    description: "Monster House is an animated film that follows three kids who discover that their neighbor's house is actually alive and hungry for anything that lands on its property. They must work together to unravel the mystery behind the house's supernatural powers and find a way to stop it before Halloween."
+    description:
+      "Monster House is an animated film that follows three kids who discover that their neighbor's house is actually alive and hungry for anything that lands on its property. They must work together to unravel the mystery behind the house's supernatural powers and find a way to stop it before Halloween.",
+  },
+  {
+    title: "The Silence of the Lambs",
+    card: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/uS9m8OBk1A8eM9I042bx8XXpqAq.jpg",
+    description:
+      "The Silence of the Lambs is a 1991 thriller where FBI trainee Clarice Starling consults with the imprisoned serial killer Dr. Hannibal Lecter to catch another serial killer, Buffalo Bill. The film is known for its suspenseful storytelling and iconic performances by Jodie Foster and Anthony Hopkins.",
   },
   {
     title: "Hocus Pocus",
     card: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/by4D4Q9NlUjFSEUA1yrxq6ksXmk.jpg",
-    description: "Hocus Pocus is a Halloween comedy set in Salem, Massachusetts, where three resurrected witch sisters seek to regain their youth by stealing children's life force. A teenager, his sister, and a crush must stop the witches' evil plan. The film is known for its humor and has become a Halloween classic."
+    description:
+      "Hocus Pocus is a Halloween comedy set in Salem, Massachusetts, where three resurrected witch sisters seek to regain their youth by stealing children's life force. A teenager, his sister, and a crush must stop the witches' evil plan. The film is known for its humor and has become a Halloween classic.",
   },
   {
     title: "The Nightmare Before Christmas",
     card: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/oQffRNjK8e19rF7xVYEN8ew0j7b.jpg",
-    description: "The Nightmare Before Christmas is a stop-motion animated musical in which Jack Skellington, the Pumpkin King, grows tired of Halloween and decides to take over Christmas, leading to a whimsical and darkly humorous adventure as he tries to celebrate the holiday in his own spooky style."
+    description:
+      "The Nightmare Before Christmas is a stop-motion animated musical in which Jack Skellington, the Pumpkin King, grows tired of Halloween and decides to take over Christmas, leading to a whimsical and darkly humorous adventure as he tries to celebrate the holiday in his own spooky style.",
   },
   {
     title: "Ghostbusters",
     card: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/7E8nLijS9AwwUEPu2oFYOVKhdFA.jpg",
-    description: "Ghostbusters is a 1984 comedy in which three scientists start a ghost-catching business in New York City. Donning proton packs and traps, they battle a surge of paranormal activity, including a powerful ghost named Gozer, in a comedic and action-packed quest to save the city from supernatural chaos."
+    description:
+      "Ghostbusters is a 1984 comedy in which three scientists start a ghost-catching business in New York City. Donning proton packs and traps, they battle a surge of paranormal activity, including a powerful ghost named Gozer, in a comedic and action-packed quest to save the city from supernatural chaos.",
   },
   {
     title: "Coraline",
     card: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4jeFXQYytChdZYE9JYO7Un87IlW.jpg",
-    description: "Coraline is a dark fantasy film about a young girl who finds a hidden door to a seemingly perfect alternate world. However, she soon discovers the sinister intentions of her Other Mother and must fight to protect her real family from being trapped in the parallel world forever."
+    description:
+      "Coraline is a dark fantasy film about a young girl who finds a hidden door to a seemingly perfect alternate world. However, she soon discovers the sinister intentions of her Other Mother and must fight to protect her real family from being trapped in the parallel world forever.",
   },
+  {
+    title: "The Exorcist",
+    card: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/4ucLGcXVVSVnsfkGtbLY4XAius8.jpg",
+    description:
+      "The Exorcist is a 1973 horror film about a young girl named Regan who becomes possessed by a demonic entity. Her mother seeks the help of two priests to perform an exorcism and save her daughter's soul. The film is a classic in the horror genre, known for its intense and terrifying portrayal of possession.",
+  },
+  {
+    title: "It's the Great Pumpkin, Charlie Brown",
+    card: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/59wp9OWexYsxlSPHYmVLsl5xlFt.jpg",
+    description:
+      "It's the Great Pumpkin, Charlie Brown centers on Linus's unwavering belief in the Great Pumpkin, a mythical figure who brings gifts to children on Halloween. While Linus waits in a pumpkin patch, the rest of the gang goes trick-or-treating and enjoys various Halloween activities.",
+  },
+  {
+    title: "Beetlejuice",
+    card: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/nnl6OWkyPpuMm595hmAxNW3rZFn.jpg",
+    description:
+      "Beetlejuice is a 1988 dark comedy directed by Tim Burton about a deceased couple who hire the chaotic ghost Betelgeuse to reclaim their home from the living. The film is known for its quirky characters, supernatural humor, and imaginative style.",
+  },
+  {
+    title: "The Haunted Mansion",
+    card: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/lGi5yio4pdDz5PkSeZCbnMQz5vK.jpg",
+    description:
+      "The Haunted Mansion follows a real estate agent, Jim Evers, and his family as they enter a mysterious and ghost-filled mansion with hopes of selling it. However, they soon find themselves entangled in a supernatural mystery and must help free the trapped spirits to break the curse on the house.",
+  },
+  {
+    title: "The Shining",
+    card: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/xazWoLealQwEgqZ89MLZklLZD3k.jpg",
+    description: "The Shining is a horror film, based on Stephen King's novel. It follows the Torrance family as they move to an isolated hotel for the winter. The father, Jack Torrance, descends into madness due to supernatural forces in the hotel, while his son, Danny, has psychic abilities."
+  }
 ];
