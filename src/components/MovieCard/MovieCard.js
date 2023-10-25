2ceef0498e0affb135fc328396f066854c715089
Chandlar Lanier <cklanier@outlook.com>
2023-10-24 22:40:03 -0400

    Movie description hover feature

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -6,9 +6,9 @@ const MovieCard = () => {
     const [hover, setHover] = useState(0);
   return (
     <div className="movie-card">
-      <div className={`movie-card__poster ${hover ? "active" : "inactive"}`} onMouseEnter={() => setHover(1)} onMouseLeave={() => setHover(0)}>
+      <div className={`movie-card__poster`} onMouseEnter={() => setHover(1)} onMouseLeave={() => setHover(0)}>
         <img alt="Movie poster"/>
-        <div className='movie-card__overlay'>
+        <div className={`movie-card__overlay ${hover ? "active" : "inactive"}`}>
             <p className='movie-card__description'>Description here</p>
         </div>
       </div>
