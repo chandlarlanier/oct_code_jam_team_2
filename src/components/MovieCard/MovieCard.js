1fecdd2f780886064f53ab158a0c8ab91e8f55a4
Chandlar Lanier <cklanier@outlook.com>
2023-10-24 20:57:11 -0400

    Star ratings

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,11 +1,19 @@
 import "./MovieCard.css";
+import StarRating from "../StarRating/StarRating";
+import { useState } from "react";
 
 const MovieCard = () => {
+    const [hover, setHover] = useState(0);
   return (
     <div className="movie-card">
-      <div className='movie-card__container'>
-        <div className="movie-card__poster">Image here</div>
+      <div className={`movie-card__poster ${hover ? "active" : "inactive"}`} onMouseEnter={() => setHover(1)} onMouseLeave={() => setHover(0)}>
+        <img alt="Movie poster"/>
+        <div className='movie-card__overlay'>
+            <p className='movie-card__description'>Description here</p>
+        </div>
       </div>
+      <div className="">Title here</div>
+      <StarRating />
     </div>
   );
 };
